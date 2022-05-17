dc855de767b474f750bb983b584cd2cdbc096f60
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-05-16 21:02:32 -0300

    sliding finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,72 @@ const showMenu = (toggleId, navId) => {
 showMenu("nav-toggle", "nav-menu");
 
 // swiper js
+let galleryThumbs = new Swiper(".gallery-thumbs", {
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+
+let galleryTop = new Swiper(".gallery-top", {
+  effect: "fade",
+  loop: true,
+
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
 
 // pop up
+const btnOpenVideo = document.querySelectorAll(".islands__video-content");
+const islandsPopup = document.getElementById("popup");
+
+function popUp() {
+  islandsPopup.classList.add("show-popup");
+}
+
+btnOpenVideo.forEach((b) => b.addEventListener("click", popUp));
+
+const btnCloseVideo = document.getElementById("popup-close");
+
+btnCloseVideo.addEventListener("click", () => {
+  islandsPopup.classList.remove("show-popup");
+});
 
 // gsap animation
+const controlImg = document.querySelectorAll(".controls__img");
+
+function scrollAnimation() {
+  gsap.from(".islands__subtitle", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+    y: -20,
+  });
+  gsap.from(".islands__title", {
+    opacity: 0,
+    duration: 0.3,
+    delay: 0.3,
+    y: -20,
+  });
+  gsap.from(".islands__description", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 0.4,
+    y: -20,
+  });
+  gsap.from(".islands__button", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5,
+    y: -20,
+  });
+  gsap.from(".islands__video-content", {
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.6,
+    y: -20,
+  });
+
+  islandsPopup.classList.remove("show-popup");
+}
+
+controlImg.forEach((c) => c.addEventListener("click", scrollAnimation));
